e68e3157d8b50228b14169740da4f2433b705e48
Ahmed Gadein Ali <ahmed.g.obied@gmail.com>
2020-12-13 22:55:20 +0200

    initial commit

---
+++ android/app/src/main/kotlin/com/uofk/handasa_project/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.uofk.handasa_project
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,48 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+        visualDensity: VisualDensity.adaptivePlatformDensity,
+      ),
+      home: MyHomePage(),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  // ====== Change this ====== //
+  String name = "Your Name"; //Change this variable to your name
+
+  @override
+  _MyHomePageState createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        leading: Icon(Icons.code_sharp),
+        title: Text("Handasa"),
+      ),
+      body: Container(
+        child: Center(
+          child: Text(
+            widget.name,
+            style: TextStyle(fontSize: 20, color: Colors.blue[900]),
+          ),
+        ),
+      ),
+    );
+  }
+}
